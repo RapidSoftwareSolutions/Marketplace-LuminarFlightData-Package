b4e542f162361dd38785f0f0929d1c25c051ef37
Igor-Rabodzei <igor.rabodzei@gmail.com>
2018-05-03 13:46:53 +0300

    fix bug spaces

--- index.js
+++ index.js
@@ -3,86 +3,96 @@
 global.PACKAGE_NAME = 'LaminarFlightData';
 global.API_ENDPOINT = 'https://api.laminardata.aero/v1';
 
-const express       = require('express'),
-    Promise         = require('bluebird'),
-    bodyParser      = require('body-parser'),
-    xmlParser       = require('xml2js').parseString,
-    API             = require('rapi-js-package'),
-    fs              = require('fs'),
-    lib             = require('./lib'),
-    _               = lib.callback;
+const express = require('express'),
+ Promise = require('bluebird'),
+ bodyParser = require('body-parser'),
+ xmlParser = require('xml2js').parseString,
+ API = require('rapi-js-package'),
+ fs = require('fs'),
+ lib = require('./lib'),
+ _ = lib.callback;
 
-const PORT          = process.env.PORT || 8080;
-const app           = express();
+const PORT = process.env.PORT || 8080;
+const app = express();
 
-app.use(bodyParser.json(({limit: '50mb'})));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.all(`/api/${PACKAGE_NAME}`, (req, res) => { res.send(metadata); });
+app.use(bodyParser.json(({
+ limit: '50mb'
+})));
+app.use(bodyParser.urlencoded({
+ limit: '50mb',
+ extended: true
+}));
+app.all(`/api/${PACKAGE_NAME}`, (req, res) => {
+ res.send(metadata);
+});
 
-let parseResponse = (res) => new Promise((resolve, reject) => {	
-		xmlParser(res, /*{ignoreAttrs: true},*/ (err, result) => {
-			if(err) reject();
-			resolve(result);
-	})
-	
+let parseResponse = (res) => new Promise((resolve, reject) => {
+ xmlParser(res, /*{ignoreAttrs: true},*/ (err, result) => {
+  if (err) reject();
+  resolve(result);
+ })
+
 })
 
 let mfile = fs.readFileSync('./metadata.json', 'utf-8'),
-    cfile = fs.readFileSync('./control.json',  'utf-8');
+ cfile = fs.readFileSync('./control.json', 'utf-8');
 
 let metadata = JSON.parse(mfile),
-    control  = JSON.parse(cfile);
+ control = JSON.parse(cfile);
 
-for(let func in control) {
-    let options = {
-        parseUri: true,
-        hasSkip:  true,
-        query:    {},
-        //debug:    true
-    };
+for (let func in control) {
+ let options = {
+  parseUri: true,
+  hasSkip: true,
+  query: {},
+  //debug:    true
+ };
 
-    let {
-        method, 
-        args,
-        tree,
-        url
-    } = control[func];
+ let {
+  method,
+  args,
+  tree,
+  url
+ } = control[func];
 
-    app.post(`/api/${PACKAGE_NAME}/${func}`, _(function* (req, res) {
-        let response;
-        let opts = {};
-        let r    = {
-            callback     : "",
-            contextWrites: {}
-        };
+ app.post(`/api/${PACKAGE_NAME}/${func}`, _(function*(req, res) {
+  let response;
+  let opts = {};
+  let r = {
+   callback: "",
+   contextWrites: {}
+  };
 
-        req.body.args = lib.clearArgs(req.body.args);
+  req.body.args = lib.clearArgs(req.body.args);
 
-        try {
-            let api = new API(API_ENDPOINT + url, {
-                headers: {
-                    'Accept': 'application/json'
-                }
-						});
+  try {
+   let api = new API(API_ENDPOINT + url, {
+    headers: {
+     'Accept': 'application/json'
+    }
+   });
 
-            for(let arg in args) {
-                let argarr      = arg.split('|');
-                opts[args[arg] + '|' + argarr[0]] = req.body.args[argarr[1]];
-						}
+   for (let arg in args) {
+    let argarr = arg.split('|');
+    opts[args[arg] + '|' + argarr[0]] = req.body.args[argarr[1]];
+   }
 
-            method == 'GET' ? options.query = opts : options.body = opts;
-						options.method = method;
+   method == 'GET' ? options.query = opts : options.body = opts;
+   options.method = method;
 
-						response              = yield api.request(options);
-						r.callback            = 'success';
-						r.contextWrites['to'] = (func == 'getFlightsByAerodrome' || func == 'getFlightsByAirline' || func == 'getFlightsByTile' || func == 'getFlightDataFirLoad' || func == 'getFlightDetailByGUFI' || func == 'getDeparturesByAerodrome' || func == 'getFlightDelayByGUFI')? response : yield parseResponse(response);
-        } catch(e) {
-						r.callback            = 'error';
-            r.contextWrites['to'] = e.status_code ? e : { status_code: "API_ERROR", status_msg: '404 Not Found. Please, check api params and try again.' };
-        }
+   response = yield api.request(options);
+   r.callback = 'success';
+   r.contextWrites['to'] = (func == 'getFlightsByAerodrome' || func == 'getFlightsByAirline' || func == 'getFlightsByTile' || func == 'getFlightDataFirLoad' || func == 'getFlightDetailByGUFI' || func == 'getDeparturesByAerodrome' || func == 'getFlightDelayByGUFI') ? response : yield parseResponse(response);
+  } catch (e) {
+   r.callback = 'error';
+   r.contextWrites['to'] = e.status_code ? e : {
+    status_code: "API_ERROR",
+    status_msg: '404 Not Found. Please, check api params and try again.'
+   };
+  }
 
-        res.status(200).send(r);
-    }))
+  res.status(200).send(r);
+ }))
 }
 
 app.listen(PORT);
